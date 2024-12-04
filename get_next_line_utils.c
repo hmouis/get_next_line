236091c44f9dd40deccec4befdf9013d3b6945e6
hmouis <houdayfamouis@gmail.com>
2024-12-04 16:25:48 +0100

    mab9ach leaks

--- get_next_line_utils.c
+++ get_next_line_utils.c
@@ -1,15 +1,26 @@
-#include "get_next_line.h"
-#include <stdio.h>
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   get_next_line_utils.c                              :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: hmouis <marvin@42.fr>                      +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/12/04 16:03:39 by hmouis            #+#    #+#             */
+/*   Updated: 2024/12/04 16:03:43 by hmouis           ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
 
-char	*ft_strchr(char *s, int c)
+#include "get_next_line.h"
+
+char	*ft_strchr(char *s, char c)
 {
 	size_t	i;
 
 	i = 0;
 	while (s && s[i] != '\0')
 	{
-		if (s[i] == (char)c)
-			return ((char *)s + (i + 1));
+		if (s[i] == c)
+			return (s + i + 1);
 		i++;
 	}
 	return (NULL);
@@ -39,28 +50,28 @@ size_t	ft_strlen(char *s)
 {
 	size_t	i;
 
-    i = 0;
-    if (!s)
-        return (0);
-    while (s[i] != '\0')
+	i = 0;
+	if (!s)
+		return (0);
+	while (s[i] != '\0')
 		i++;
 	return (i);
 }
 
-char    *ft_strcpy_nl(char *line, char *str)
+char	*ft_strcpy_nl(char *line, char *str)
 {
-    int i;
+	int	i;
 
-    i = 0;
-    if (!str)
-        return (NULL);
-    while (str && str[i] != '\n')
-    {
-        line[i] = str[i];
-        i++;
-    }
-    line[i] = '\0';
-    return (line);
+	i = 0;
+	if (!str)
+		return (NULL);
+	while (str && str[i] != '\n')
+	{
+		line[i] = str[i];
+		i++;
+	}
+	line[i] = '\0';
+	return (line);
 }
 
 char	*ft_strjoin(char *s1, char *s2)
@@ -69,23 +80,23 @@ char	*ft_strjoin(char *s1, char *s2)
 	size_t	i;
 	size_t	j;
 
-
 	new = malloc(sizeof(char) * (ft_strlen(s1) + ft_strlen(s2) + 1));
 	if (!new)
 		return (NULL);
 	i = ft_strlen(s1);
 	j = 0;
-    while (s1 && s1[j] != '\0')
-    {
-        new[j] = s1[j];
-        j++;
-    }
-    j = 0;
+	while (s1 && s1[j] != '\0')
+	{
+		new[j] = s1[j];
+		j++;
+	}
+	j = 0;
 	while (s2[j] != '\0')
 	{
-        new[i + j] = s2[j];
+		new[i + j] = s2[j];
 		j++;
 	}
 	new[i + j] = '\0';
+	free(s1);
 	return (new);
 }
