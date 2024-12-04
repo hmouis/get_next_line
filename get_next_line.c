8c20a68c6dff15b8c208409815d089a33ebe3b33
Hodaifa Mouis <hmouis@e1r12p10.1337.ma>
2024-12-04 16:25:48 +0100

    mab9ach leaks

--- get_next_line.c
+++ get_next_line.c
@@ -1,66 +1,91 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   get_next_line.c                                    :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: hmouis <marvin@42.fr>                      +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/12/04 16:03:23 by hmouis            #+#    #+#             */
+/*   Updated: 2024/12/04 16:03:26 by hmouis           ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
 #include "get_next_line.h"
 
-char *ft_strcpy(char *str, char *after_newline)
+char	*ft_strcpy(char *str, char *after_newline)
 {
-    int i;
+	int	i;
 
-    i = 0;
-    while (after_newline[i])
-    {
-        str[i] = after_newline[i];
-        i++;
-    }
-    str[i] = '\0';
-    return (str);
+	i = 0;
+	while (after_newline[i])
+	{
+		str[i] = after_newline[i];
+		i++;
+	}
+	str[i] = '\0';
+	return (str);
 }
 
-char *get_line(char *str, char *after_newline)
+char	*get_line(char *str, char *after_newline)
 {
-    char *line;
+	char	*line;
 
-    if (!after_newline)
-    {
-        free(str);
-        return (NULL);
-    }
-    line = malloc(sizeof(char) * (ft_strlen(str) - ft_strlen(after_newline)));
-    if (!line)
-    {
-        free(str);
-        return (NULL);
-    }
-    line = ft_strcpy_nl(line, str);
-    return (line);
+	if (!after_newline)
+	{
+		free(str);
+		return (NULL);
+	}
+	line = malloc(sizeof(char) * (ft_strlen(str) - ft_strlen(after_newline)));
+	if (!line)
+	{
+		free(str);
+		return (NULL);
+	}
+	line = ft_strcpy_nl(line, str);
+	return (line);
 }
 
-char *get_next_line(int fd)
+char	*fill_str(char *str, char *buf, int fd)
 {
-    char *after_newline;
-    char *line;
-    static char *str;
-    char *buf;
-    ssize_t count;
+	ssize_t	count;
 
-    if (fd < 0 || BUFFER_SIZE <= 0)
-        return (NULL);
-    buf = malloc(sizeof(char) * (BUFFER_SIZE + 1));
-    if (!buf)
-        return (free(str), NULL);
-    while (!ft_strchr(buf, '\n'))
-    {
-        count = read(fd, buf, BUFFER_SIZE);
-        if (count <= 0)
-            break;
-        buf[count] = '\0';
-        str = ft_strjoin(str, buf);
-        if (!str)
-            return (free(str), NULL);
-    }
-    free(buf);
-    after_newline = ft_strchr(str, '\n');
-    line = get_line(str, after_newline);
-    if (!line)
-        return (NULL);
-    str = ft_strcpy(str, after_newline);
-    return (line);
+	while (!ft_strchr(buf, '\n'))
+	{
+		count = read(fd, buf, BUFFER_SIZE);
+		if (count == 0)
+			break ;
+		buf[count] = '\0';
+		str = ft_strjoin(str, buf);
+		if (!str)
+			return (free(str), NULL);
+	}
+	free(buf);
+	return (str);
+}
+
+char	*get_next_line(int fd)
+{
+	char		*after_newline;
+	char		*line;
+	static char	*str;
+	char		*buf;
+	ssize_t		count;
+
+	if (fd < 0 || BUFFER_SIZE <= 0)
+		return (NULL);
+	buf = malloc(sizeof(char) * (BUFFER_SIZE + 1));
+	if (!buf)
+		return (free(str), NULL);
+	count = 0;
+	str = fill_str(str, buf, fd);
+	if (!str)
+	{
+		return (NULL);
+	}
+	after_newline = ft_strchr(str, '\n');
+	line = get_line(str, after_newline);
+	if (!line)
+		return (NULL);
+	str = ft_strcpy(str, after_newline);
+	return (line);
 }
--- get_next_line.h
+++ get_next_line.h
@@ -1,19 +1,31 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   get_next_line.h                                    :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: hmouis <marvin@42.fr>                      +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/12/04 16:03:32 by hmouis            #+#    #+#             */
+/*   Updated: 2024/12/04 16:03:33 by hmouis           ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
 #ifndef GET_NEXT_LINE_H
 # define GET_NEXT_LINE_H
 
-#ifndef BUFFER_SIZE
-# define BUFFER_SIZE 10
-#endif
+# ifndef BUFFER_SIZE
+#  define BUFFER_SIZE 10
+# endif
 
-# include <stdlib.h>
 # include <fcntl.h>
+# include <stdlib.h>
 # include <unistd.h>
 
-char	*ft_strchr(char *s, int c);
+char	*ft_strchr(char *s, char c);
 char	*ft_strdup(char *s1);
 size_t	ft_strlen(char *s);
 char	*ft_strjoin(char *s1, char *s2);
-char *get_next_line(int fd);
-char *ft_strcpy_nl(char *line, char *str);
+char	*get_next_line(int fd);
+char	*ft_strcpy_nl(char *line, char *str);
 
 #endif
